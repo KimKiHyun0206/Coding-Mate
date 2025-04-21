49dfeab9deec8b24f456b79cc242882c227b44ec
Duskafka <rlarlgus0206@naver.com>
2025-04-21 22:49:00 +0900

    init Commit

---
+++ src/main/java/com/codingMate/CodingMateApplication.java
@@ -0,0 +1,13 @@
+package com.codingMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingMateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingMateApplication.class, args);
+    }
+
+}
