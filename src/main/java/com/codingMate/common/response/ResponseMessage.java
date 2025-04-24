1fe06734f0562f3a7f8d1229266f880d8d134127
Duskafka <rlarlgus0206@naver.com>
2025-04-24 18:20:01 +0900

    feat(ResponseDto): 요청에 대한 응답으로 HTTP 응답 코드와 body에 담을 수 있도록 함

---
+++ src/main/java/com/codingMate/common/response/ResponseMessage.java
@@ -0,0 +1,26 @@
+package com.codingMate.common.response;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ResponseMessage {
+
+    SUCCESS(HttpStatus.OK,"SUCCESS"),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST,"BAD REQUEST"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"INTERNAL SERVER ERROR"),
+    //PROGRAMMER
+    ;
+
+    public final static String SUCCESS_MESSAGE = "SUCCESS";
+    private final static String NOT_FOUND_MESSAGE = "NOT FOUND";
+
+
+    private final HttpStatus status;
+    private final String message;
+
+    ResponseMessage(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
