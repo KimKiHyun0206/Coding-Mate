a34418dab587fbcec9b059cd58c96f9b69a3ec77
Duskafka <rlarlgus0206@naver.com>
2025-05-16 14:28:45 +0900

    fix: 만료된 토큰이더라도 통과시키도록 변경함
    만료된 토큰은 다시 발급받아야 하기 때문임

--- src/main/java/com/codingMate/jwt/JwtFilter.java
+++ src/main/java/com/codingMate/jwt/JwtFilter.java
@@ -1,8 +1,12 @@
 package com.codingMate.jwt;
 
+import com.codingMate.exception.exception.jwt.ExpiredTokenException;
+import com.codingMate.util.JwtUtil;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
@@ -13,40 +17,36 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
+
 import java.io.IOException;
 
 @Slf4j
+@RequiredArgsConstructor
 public class JwtFilter extends GenericFilterBean {
-   public static final String AUTHORIZATION_HEADER = "Coding-Mate-Auth";
-   private TokenProvider tokenProvider;
-   public JwtFilter(TokenProvider tokenProvider) {
-      this.tokenProvider = tokenProvider;
-   }
 
-   @Override
-   public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-      HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-      String jwt = resolveToken(httpServletRequest);
-      String requestURI = httpServletRequest.getRequestURI();
+    @Value("${jwt.header}")
+    private String AUTHORIZATION_HEADER;
+    private final TokenProvider tokenProvider;
 
-      if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
-         Authentication authentication = tokenProvider.getAuthentication(jwt);
-         SecurityContextHolder.getContext().setAuthentication(authentication);
-         log.debug("Security Context에 '{}' 인증 정보를 저장했습니다, uri: {}", authentication.getName(), requestURI);
-      } else {
-         log.debug("유효한 JWT 토큰이 없습니다, uri: {}", requestURI);
-      }
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+        String jwt = JwtUtil.getAccessTokenFromHttpServletRequest(httpServletRequest);
+        String requestURI = httpServletRequest.getRequestURI();
 
-      filterChain.doFilter(servletRequest, servletResponse);
-   }
-
-   private String resolveToken(HttpServletRequest request) {
-      String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
-
-      if (StringUtils.hasText(bearerToken)) {
-         return bearerToken;
-      }
-
-      return null;
-   }
+        try {
+            if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
+                Authentication authentication = tokenProvider.getAuthentication(jwt);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+                log.debug("Security Context에 '{}' 인증 정보를 저장했습니다, uri: {}", authentication.getName(), requestURI);
+            } else {
+                log.debug("유효한 JWT 토큰이 없습니다, uri: {}", requestURI);
+            }
+            filterChain.doFilter(servletRequest, servletResponse);
+        } catch (ExpiredTokenException e) { //만료된 토큰일 시
+            log.info("만료된 JWT 토큰입니다, uri: {}", requestURI);
+            httpServletResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        }
+    }
 }
