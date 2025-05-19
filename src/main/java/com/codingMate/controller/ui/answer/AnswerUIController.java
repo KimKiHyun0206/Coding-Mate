b2df46748180a93b74a4b02c3b91bc25217bfa17
Duskafka <rlarlgus0206@naver.com>
2025-05-19 12:58:17 +0900

    fix: 문제 리스트를 모달로 열지 않고 새 창을 열어서 확인할 수 있도록 함
    모달로 창 위에 창을 열어서 사용하는 방식을 사용하면 추후 댓글이나 다른 기능을 추가했을 때 변경이 필요한데 이를 미리 변경함

--- src/main/java/com/codingMate/controller/ui/answer/AnswerUIController.java
+++ src/main/java/com/codingMate/controller/ui/answer/AnswerUIController.java
@@ -13,7 +13,7 @@ public class AnswerUIController {
 
     @RequestMapping("/list")
     public String list() {
-        return "answer/list_new";
+        return "answer/list";
     }
 
     @RequestMapping("/{id}")
