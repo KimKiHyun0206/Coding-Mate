f132419d542117dd089250ea06e7467d93213216
Duskafka <rlarlgus0206@naver.com>
2025-05-22 19:33:20 +0900

    fix: 엔티티를 생성할 때 programmer를 받도록 함

--- src/main/java/com/codingMate/answer/dto/request/AnswerCreateRequest.java
+++ src/main/java/com/codingMate/answer/dto/request/AnswerCreateRequest.java
@@ -2,14 +2,16 @@ package com.codingMate.answer.dto.request;
 
 import com.codingMate.answer.domain.Answer;
 import com.codingMate.answer.domain.vo.LanguageType;
+import com.codingMate.programmer.domain.Programmer;
 
 public record AnswerCreateRequest(String code, String title, String explanation, LanguageType languageType, Long backjoonId) {
-    public Answer toEntity(){
+    public Answer toEntity(Programmer programmer){
         return Answer.builder()
                 .code(code)
                 .title(title)
                 .explanation(explanation)
                 .languageType(languageType)
+                .programmer(programmer)
                 .backJoonId(backjoonId)
                 .build();
     }
