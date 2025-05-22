f3b7ed6818b2812a20c8b8e8b5c94353ee98b244
Duskafka <rlarlgus0206@naver.com>
2025-05-22 18:22:57 +0900

    update: Programmer가 생성되지 않음을 나타내는 ErrorMessage 추가

--- src/main/java/com/codingMate/exception/exception/programmer/ProgrammerNotCreateException.java
+++ src/main/java/com/codingMate/exception/exception/programmer/ProgrammerNotCreateException.java
@@ -1,9 +1,18 @@
 package com.codingMate.exception.exception.programmer;
 
 import com.codingMate.exception.BusinessException;
+import com.codingMate.exception.dto.ErrorMessage;
 
 public class ProgrammerNotCreateException extends BusinessException {
-    public ProgrammerNotCreateException(String message) {
+    public ProgrammerNotCreateException(ErrorMessage message) {
         super(message);
+    }
+
+    public ProgrammerNotCreateException(ErrorMessage message, String reason) {
+        super(message, reason);
+    }
+
+    public ProgrammerNotCreateException(String reason) {
+        super(reason);
     }
 }
