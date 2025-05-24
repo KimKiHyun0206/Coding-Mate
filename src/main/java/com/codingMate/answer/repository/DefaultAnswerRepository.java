a10778923f9baee745e10284cce561d4e3fa1a94
Duskafka <rlarlgus0206@naver.com>
2025-05-24 23:34:07 +0900

    delete: 불필요한 메소드 삭제

--- src/main/java/com/codingMate/answer/repository/DefaultAnswerRepository.java
+++ src/main/java/com/codingMate/answer/repository/DefaultAnswerRepository.java
@@ -4,9 +4,6 @@ import com.codingMate.answer.domain.Answer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface DefaultAnswerRepository extends JpaRepository<Answer, Long> {
-    List<Answer> readAnswersByProgrammerId(Long programmerId);
 }
