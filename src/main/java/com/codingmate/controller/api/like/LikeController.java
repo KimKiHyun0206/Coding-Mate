07e5bb4d04d083f7a7a44e98f5f6a153412409c3
Duskafka <rlarlgus0206@naver.com>
2025-06-01 12:22:38 +0900

    fix: api 엔드포인트 수정

--- src/main/java/com/codingmate/controller/api/like/LikeController.java
+++ src/main/java/com/codingmate/controller/api/like/LikeController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/v1/vote")
+@RequestMapping("/api/v1/like")
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public class LikeController {
     private final LikeService likeService;
