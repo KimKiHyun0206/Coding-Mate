f727f9a230df4f27f347de3a735dafa812e04cfb
Duskafka <rlarlgus0206@naver.com>
2025-06-02 20:57:33 +0900

    fix: 리펙토링 중 URL을 잘못 설정했던 것을 수정함

--- src/main/java/com/codingmate/controller/ui/AuthUIController.java
+++ src/main/java/com/codingmate/controller/ui/AuthUIController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/login")
+@RequestMapping
 public class AuthUIController {
     @RequestMapping("/login")
     public String login() {
