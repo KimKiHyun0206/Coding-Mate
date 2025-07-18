68685e76b4436ec8dc1e8625b4f90c60916bb8da
Duskafka <rlarlgus0206@naver.com>
2025-07-18 14:32:59 +0900

    feat(Email): 이메일 인증을 진행해줄 컨트롤러
    이메일 인증 요청을 받으면 요청을 받아 데이터베이스에서 토큰이 유효한지 검증하고 응답을 반환해줄 컨트롤러.

---
+++ src/main/java/com/codingmate/controller/api/EmailController.java
@@ -0,0 +1,33 @@
+package com.codingmate.controller.api;
+
+import com.codingmate.email.dto.EmailVerificationTokenResponse;
+import com.codingmate.email.service.EmailService;
+import com.codingmate.email.service.EmailVerificationValidator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 이메일 인증을 실행해줄 컨트롤러.
+ *
+ * @author duskafka
+ * */
+@RestController
+@RequestMapping("/api/v1/email")
+@RequiredArgsConstructor
+public class EmailController {
+    private final EmailService emailService;
+    private final EmailVerificationValidator emailVerificationValidator;
+
+    @PostMapping("/verify-email")
+    public ResponseEntity<?> verifyEmail(@RequestParam("token") String token) {
+        EmailVerificationTokenResponse verification = emailService.findByToken(token);
+
+        boolean isEmailVerificationValid = emailVerificationValidator.isValidEmailToken(verification);
+
+        return isEmailVerificationValid ?
+                ResponseEntity.ok(HttpStatus.OK) :
+                ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+}
