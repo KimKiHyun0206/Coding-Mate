514d4c3f914344e38b29d8cf390ed0e3257cc1ba
Duskafka <rlarlgus0206@naver.com>
2025-07-19 21:58:54 +0900

    feat(Email): 이메일 인증 도메인
    - 도메인 내부에서 도메인을 생성하도록 함.
    - 인증 토큰이 만료되었는지 확인하는 메소드 추가.
    - 인증 완료 처리를 하기 위한 메소드 추가.

--- src/main/java/com/codingmate/email/domain/EmailVerificationToken.java
+++ src/main/java/com/codingmate/email/domain/EmailVerificationToken.java
@@ -4,15 +4,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 /**
  * 이메일 인증 정보를 데이터베이스에 저장하는 도메인.
  * */
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class EmailVerificationToken {
     /** PK */
     @Id
@@ -30,4 +34,25 @@ public class EmailVerificationToken {
 
     /** 인증이 완료되었는지 여부 */
     private boolean verified;
+
+    public EmailVerificationToken(String email) {
+        this.email = email;
+        token = UUID.randomUUID().toString();
+        expiry = LocalDateTime.now().plusMinutes(10);
+        verified = false;
+    }
+
+    /**
+     * 토큰이 만료되었는지 확인하는 메소드
+     * */
+    public boolean isExpired() {
+        return LocalDateTime.now().isAfter(expiry);
+    }
+
+    /**
+     * 인증 완료 처리를 하기 위한 메소드
+     * */
+    public void markAsVerified() {
+        this.verified = true;
+    }
 }
