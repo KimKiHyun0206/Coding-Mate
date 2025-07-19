3158d4387735620238e7846960369cc5693c2e1d
Duskafka <rlarlgus0206@naver.com>
2025-07-19 21:56:00 +0900

    feat(Email): 이메일 인증 및 인증 요청 API 구현
    POST 요청으로 인증할 이메일에 이메일을 보내고, GET 요청으로 받은 이메일에서 인증을 진행할 수 있도록 한다.

--- src/main/java/com/codingmate/controller/api/EmailController.java
+++ src/main/java/com/codingmate/controller/api/EmailController.java
@@ -1,7 +1,9 @@
 package com.codingmate.controller.api;
 
 import com.codingmate.email.dto.EmailVerificationTokenResponse;
+import com.codingmate.email.service.EmailSendService;
 import com.codingmate.email.service.EmailService;
+import com.codingmate.email.service.EmailVerificationTokenGenerator;
 import com.codingmate.email.service.EmailVerificationValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -12,15 +14,17 @@ import org.springframework.web.bind.annotation.*;
  * 이메일 인증을 실행해줄 컨트롤러.
  *
  * @author duskafka
- * */
+ */
 @RestController
 @RequestMapping("/api/v1/email")
 @RequiredArgsConstructor
 public class EmailController {
     private final EmailService emailService;
     private final EmailVerificationValidator emailVerificationValidator;
+    private final EmailSendService emailSendService;
+    private final EmailVerificationTokenGenerator emailVerificationTokenGenerator;
 
-    @PostMapping("/verify-email")
+    @GetMapping("/verify-email")
     public ResponseEntity<?> verifyEmail(@RequestParam("token") String token) {
         EmailVerificationTokenResponse verification = emailService.findByToken(token);
 
@@ -29,5 +33,12 @@ public class EmailController {
         return isEmailVerificationValid ?
                 ResponseEntity.ok(HttpStatus.OK) :
                 ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @PostMapping("/verify-email")
+    public ResponseEntity<?> sendEmail(@RequestParam("email") String email) {
+        String token = emailVerificationTokenGenerator.generateEmailVerificationToken(email);
+        emailSendService.sendHtmlEmail(email, token);
+        return ResponseEntity.ok().build();
     }
 }
