3651ad7ee371772415cc630716baebeec34328d6
Duskafka <rlarlgus0206@naver.com>
2025-07-19 21:56:44 +0900

    feat(Email): 이메일 프로퍼티
    발신자 주소, 이메일 제목, 이메일 인증 URL, 이메일 메시지 템플릿을 프로퍼티로 등록.

---
+++ src/main/java/com/codingmate/config/properties/EmailProperties.java
@@ -0,0 +1,25 @@
+package com.codingmate.config.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * 회원가입 이메일 설정을 해줄 프로퍼티
+ *
+ * @author duskafka
+ * */
+@ConfigurationProperties(prefix = "register-email")
+public record EmailProperties (
+
+        /** 발신자 주소 */
+        String username,
+
+        /** 이메일 제목 */
+        String subject,
+
+        /** 이메일 인증 URL 앞부분 */
+        String url,
+
+        /** 이메일 메시지 템플릿 */
+        String template
+) {
+}
