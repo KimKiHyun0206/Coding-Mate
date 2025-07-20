77fd79f1a7091aca330e4272dd21ad7c82904ca5
Duskafka <rlarlgus0206@naver.com>
2025-07-20 14:46:27 +0900

    feat(Email): 이메일 UI 컨트롤러
    - HTML에서 타임리프로 처리하기 위해 model에 success값을 넣어준다.
    - 추후 JS로 변경해야 할 가능성 있음.

---
+++ src/main/java/com/codingmate/controller/ui/EmailUIController.java
@@ -0,0 +1,15 @@
+package com.codingmate.controller.ui;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class EmailUIController {
+    @GetMapping("/verification-result")
+    public String showVerificationResult(@RequestParam("success") boolean success, Model model) {
+        model.addAttribute("success", success);
+        return "email/email_verification";
+    }
+}
