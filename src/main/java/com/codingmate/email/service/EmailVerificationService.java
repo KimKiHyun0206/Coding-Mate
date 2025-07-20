4a2a8d278b0560c43dff5f890713cd32c0d9d42b
Duskafka <rlarlgus0206@naver.com>
2025-07-20 14:45:35 +0900

    feat(Email): 이메일 인증 메소드 추가
    - 이메일 인증 요청이 들어오면 데이터베이스에서 조회하여 인증 상태를 true로 바꿀 수 있도록 함.
    - 메소드에 로깅 추가

--- src/main/java/com/codingmate/email/service/EmailVerificationService.java
+++ src/main/java/com/codingmate/email/service/EmailVerificationService.java
@@ -5,9 +5,11 @@ import com.codingmate.email.repository.EmailVerificationTokenRepository;
 import com.codingmate.exception.dto.ErrorMessage;
 import com.codingmate.exception.exception.email.NotFoundEmailVerificationTokenException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class EmailVerificationService {
@@ -15,11 +17,25 @@ public class EmailVerificationService {
 
     @Transactional(readOnly = true)
     public boolean isVerified(String email) {
+        log.debug("[EmailVerificationService] isVerified({})", email);
         EmailVerificationToken verificationToken = emailVerificationTokenRepository.findByEmail(email).orElseThrow(
                 () -> new NotFoundEmailVerificationTokenException(
                         ErrorMessage.NOT_FOUND_EMAIL_VERIFICATION,
                         String.format("요청한 이메일(%s)을 데이터베이스에서 조회하지 못했습니다", email)
                 ));
+        log.info("[EmailVerificationService] 이메일이 {}: email={}", verificationToken.isVerified() ? "인증 되었습니다." : "인증되지 않았습니다.", email);
         return verificationToken.isVerified();
+    }
+
+    @Transactional
+    public void verify(String token) {
+        log.debug("[EmailVerificationService] verify({})", token);
+        EmailVerificationToken verificationToken = emailVerificationTokenRepository.findByToken(token).orElseThrow(
+                () -> new NotFoundEmailVerificationTokenException(
+                        ErrorMessage.NOT_FOUND_EMAIL_VERIFICATION,
+                        String.format("요청한 이메일(토큰:%s)을 데이터베이스에서 조회하지 못했습니다", token)
+                ));
+        verificationToken.markAsVerified();
+        log.info("[EmailVerificationService] 이메일 인증이 정상적으로 처리되었습니다: email={}", verificationToken.getEmail());
     }
 }
