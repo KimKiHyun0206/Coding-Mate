2cbcbd9a24eb15cc62a2854a9bd65de123b431d0
Duskafka <rlarlgus0206@naver.com>
2025-07-20 15:53:48 +0900

    delete(Email): 사용하지 않는 Querydsl 레포지토리 삭제

--- src/main/java/com/codingmate/email/service/EmailService.java
+++ src/main/java/com/codingmate/email/service/EmailService.java
@@ -2,7 +2,6 @@ package com.codingmate.email.service;
 
 import com.codingmate.email.dto.EmailVerificationTokenResponse;
 import com.codingmate.email.repository.EmailVerificationTokenRepository;
-import com.codingmate.email.repository.EmailVerificationTokenWriteRepository;
 import com.codingmate.exception.dto.ErrorMessage;
 import com.codingmate.exception.exception.email.NotFoundEmailVerificationTokenException;
 import lombok.RequiredArgsConstructor;
@@ -13,7 +12,6 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class EmailService {
     private final EmailVerificationTokenRepository emailRepository;
-    private final EmailVerificationTokenWriteRepository emailTokenRepository;
 
     @Transactional(readOnly = true)
     public EmailVerificationTokenResponse findByToken(String token) {
@@ -23,10 +21,5 @@ public class EmailService {
                         ErrorMessage.NOT_FOUND_EMAIL_VERIFICATION,
                         String.format("요청한 토큰(%s)로 데이터베이스에서 이메일 인증 토큰 조회를 실패했습니다", token)
                 ));
-    }
-
-    @Transactional
-    public void saveEmailVerificationToken(Long id) {
-        long l = emailTokenRepository.verificationToken(id);
     }
 }
